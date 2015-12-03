14082786dfb7362af10861c7199981c28aa7c1d8
unknown <zhuhuotui@gmail.com>
2015-12-03 13:29:08 +0800

    setting image

--- texture/appicon.sh
+++ texture/appicon.sh
@@ -1,7 +1,18 @@
 #/bin/sh
-imageresizer -i $1x512.png -s 180x180 -png $1x180.png 
-imageresizer -i $1x512.png -s 120x120 -png $1x120.png 
-imageresizer -i $1x512.png -s 87x87 -png $1x87.png 
-imageresizer -i $1x512.png -s 80x80 -png $1x80.png 
-imageresizer -i $1x512.png -s 58x58 -png $1x58.png 
+#imageresizer -i $1x512.png -s 180x180 -png $1x180.png 
+#imageresizer -i $1x512.png -s 120x120 -png $1x120.png 
+#imageresizer -i $1x512.png -s 87x87 -png $1x87.png 
+#imageresizer -i $1x512.png -s 80x80 -png $1x80.png 
+#imageresizer -i $1x512.png -s 58x58 -png $1x58.png 
+imageresizer -i about100x60.png -s 75x45 -png about75x45.png 
+imageresizer -i about100x60.png -s 50x30 -png about50x30.png 
+imageresizer -i stamp100x60.png -s 75x45 -png stamp75x45.png 
+imageresizer -i stamp100x60.png -s 50x30 -png stamp50x30.png 
+imageresizer -i commission100x60.png -s 75x45 -png commission75x45.png 
+imageresizer -i commission100x60.png -s 50x30 -png commission50x30.png 
+imageresizer -i version100x60.png -s 75x45 -png version75x45.png 
+imageresizer -i version100x60.png -s 50x30 -png version50x30.png 
+imageresizer -i transfer100x60.png -s 75x45 -png transfer75x45.png 
+imageresizer -i transfer100x60.png -s 50x30 -png transfer50x30.png 
+
 
